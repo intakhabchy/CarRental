c3078064c161abfeab501e827581c1f6fd0ab236
intakhabchy <intakhab.chy@gmail.com>
2024-09-21 13:56:50 +0600

    heading change

--- resources/views/admin/customerlist.blade.php
+++ resources/views/admin/customerlist.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Car List for Admin') }}
+            {{ __('Customer List for Admin') }}
         </h2>
     </x-slot>
 
